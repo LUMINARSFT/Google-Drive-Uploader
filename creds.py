33285f3c019cb88bb5aaff1feeb12304b9786c5d
LUMINARSFT <91069152+LUMINARSFT@users.noreply.github.com>
2023-01-03 16:30:13 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "5874852182:AAEiAKh6sN9y-Aojue5igsmdXL6B_OcdGPY"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1q7a_r-NxFGnDdDP2qi47eHQ6Z_maK7pP"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0ADZRSnybC5bMUk9PVA"
     
     
     
